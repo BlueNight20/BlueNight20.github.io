f7de3cedf79609e64bee8026af55a3768dead4dd
ssr <2039640969@qq.com>
2026-09-03 09:37:00 +0800

    verify aboutPage

--- src/config.ts
+++ src/config.ts
@@ -8,7 +8,7 @@ import type {
 import { LinkPreset } from "./types/config";
 
 export const siteConfig: SiteConfig = {
-	title: "BlueNight's Blog",
+	title: "'s Blog",
 	subtitle: "安全研究 · 技术笔记",
 	lang: "zh_CN", // Language code, e.g. 'en', 'zh_CN', 'ja', etc.
 	themeColor: {
